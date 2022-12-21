582d320637aec92d86f20cba95e9785b735919bd
lucasschweitzer <110677834+lucasschweitzer@users.noreply.github.com>
2022-12-21 09:24:51 -0300

    First files uploaded

---
+++ Program.cs
@@ -0,0 +1,21 @@
+
+using Banco.Modelo;
+
+namespace Banco
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            Conta c;
+            ApplicationConfiguration.Initialize();
+            Application.Run(new telaPrincipal(c.Nome));
+        }
+    }
+}
