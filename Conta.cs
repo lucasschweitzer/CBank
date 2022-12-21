8477ad4bed61b41a000775b917392cdf263f3f55
lucasschweitzer <110677834+lucasschweitzer@users.noreply.github.com>
2022-12-21 09:31:22 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Modelo
+{
+    public class Conta
+    {
+        string nome;
+        string sobrenome;
+        string email;
+        int telefone;
+        int cpf;
+        Double saldo;
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Sobrenome { get => sobrenome; set => sobrenome = value; }
+        public string Email { get => email; set => email = value; }
+        public int Telefone { get => telefone; set => telefone = value; }
+        public double Saldo { get => saldo; set => saldo = value; }
+        public int Cpf { get => cpf; set => cpf = value; }
+    }
+    }
---
+++ TelaPrincipal.cs
@@ -0,0 +1,30 @@
+using Banco.Modelo;
+
+namespace Banco
+{
+    public partial class telaPrincipal : Form
+    {
+        public telaPrincipal(Conta c)
+        {
+            InitializeComponent();
+        }
+
+        ControlaConta ctrlConta = new ControlaConta();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrarSaldo_Click(object sender, EventArgs e)
+        {
+            
+                
+        }
+
+        private void telaPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
